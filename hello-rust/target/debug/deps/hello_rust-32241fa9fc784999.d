e3bb9a7adf42c46e3ee84ae0a8d826fc69bdcab2
Andri Yabu Ndapa Rehing <andriyabu@gmail.com>
2023-04-01 11:57:03 +0700

    first hello rust project

---
+++ hello-rust/target/debug/deps/hello_rust-32241fa9fc784999.d
@@ -0,0 +1,5 @@
+/home/whoami/Belajar/Rust/Pemrograman-rust-dasar-novalagung/hello-rust/target/debug/deps/hello_rust-32241fa9fc784999.rmeta: src/main.rs
+
+/home/whoami/Belajar/Rust/Pemrograman-rust-dasar-novalagung/hello-rust/target/debug/deps/hello_rust-32241fa9fc784999.d: src/main.rs
+
+src/main.rs:
---
+++ hello-rust/target/debug/deps/hello_rust-af1e23fd61a48e16.d
@@ -0,0 +1,5 @@
+/home/whoami/Belajar/Rust/Pemrograman-rust-dasar-novalagung/hello-rust/target/debug/deps/hello_rust-af1e23fd61a48e16.rmeta: src/main.rs
+
+/home/whoami/Belajar/Rust/Pemrograman-rust-dasar-novalagung/hello-rust/target/debug/deps/hello_rust-af1e23fd61a48e16.d: src/main.rs
+
+src/main.rs:
