fe52c6fd892717e9d2c4ca0ace6dcc84b0723ea9
Andri Yabu Ndapa Rehing <andriyabu@gmail.com>
2023-04-01 12:12:24 +0700

    variable with predefined value and {} as string literal

---
+++ variable/src/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+    // variable with predefined value
+    let variable_name = "predefined value";
+
+    // {} called string literal
+    // printl!() is not a function but macro identified by ! signed
+    println!("{}", variable_name);
+}
